2143eaec807bf1fd7e4c9b8e60221c61dd099191
Hiro Asari <asari.ruby@gmail.com>
2012-11-07 15:27:21 -0500

    More intelligent and less invasive wrapper

--- segments/weather_wrapper.sh
+++ segments/weather_wrapper.sh
@@ -1,6 +1,35 @@
-#! /usr/local/bin/bash
+#! /usr/bin/env bash
 if [[ -s $HOME/.rvm/scripts/rvm ]]; then
   source $HOME/.rvm/scripts/rvm
 fi
-cd `dirname $0`
-rvm 1.9.3 do bundle exec $PWD/weather_openweathermap.rb
+
+tmp_file="${tp_tmpdir}/weather.txt"
+
+if [ -f $tmp_file ]; then
+  if [ "$PLATFORM" == 'mac' ]; then
+    last_update=$(stat -f "%m" $tmp_file)
+  else
+    last_update=$(stat -c "%Y" $tmp_file)
+  fi
+  time_now=$(date +%s)
+  update_period=900 #15 minutes
+  
+  up_to_date=$(echo "(${time_now}-${last_update}) < ${update_period}" | bc)
+  if [ "$up_to_date" -eq 1 ]; then
+    weather=$(cat ${tmp_file})
+  fi
+fi
+
+if [ -z "$weather" ]; then
+  cd `dirname $0`
+  weather=$(rvm 1.9.3 do bundle exec $PWD/weather_openweathermap.rb)
+  if [ "$?" -eq "0" ]; then
+    echo "${weather}" > $tmp_file
+  elif [ -f "${tmp_file}" ]; then
+    weather=$(cat "${tmp_file}")
+  fi
+fi
+
+if [ -n "$weather" ]; then
+  echo "$weather"
+fi
